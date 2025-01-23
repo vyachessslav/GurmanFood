07e46d83934342cf70c3aea714c6e6959a894fbc
vecheslav <vezhur2003@gmail.com>
2025-01-23 03:32:12 +0300

    пункт 10

--- src/main/java/com/gmail/v/c/charkin/gurmanfood/configuration/JwtTokenProvider.java
+++ src/main/java/com/gmail/v/c/charkin/gurmanfood/configuration/JwtTokenProvider.java
@@ -5,7 +5,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -27,18 +26,5 @@ public class JwtTokenProvider {
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
-    }
-
-    public String getUserNameFromJwtToken(String token) {
-        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
-    }
-
-    public boolean validateJwtToken(String authToken) {
-        try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
     }
 }
